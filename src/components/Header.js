528489a2247362fa1c542eb7e1d1775cb80e672a
oxoROBOTOoxo <amassoud7@gmail.com>
2024-08-27 18:03:49 -0400

    refactor: Remove navigation links from Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,6 @@ const Header = () => {
   return (
     <header className="app-header">
       <h1>Decision Maker App</h1>
-      <nav>
-        <a href="#enter-options">Enter Options</a>
-        <a href="#make-selection">Make Selection</a>
-      </nav>
       <ThemeToggle />
     </header>
   );
